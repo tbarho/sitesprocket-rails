b52fb389265cc281fbc84c65806fb1e9ba70af2a
Ty Barho <t.barho@gmail.com>
2012-01-09 05:35:37 +0000

    first commit with notes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SitesprocketRails::Application.config.secret_token = 'c46c3aeb3d61a7e5c4531a6ad6a08ef8168e66a5b935108236d93f73b47b6193d05c201ca147138b350fa88e47714b57aa2d4b37861cc71cb40017c04332e8cb'
